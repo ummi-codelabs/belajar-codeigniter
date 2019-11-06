c8c2634159e64d77709a281df465877fbdca591d
Gun Gun Priatna <gungunpriatna02@gmail.com>
2019-11-07 05:48:44 +0700

    code untuk pertemuan 6

---
+++ codelabs/pertemuan_6/aplikasi_ci/application/views/layout/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title><?php echo $title; ?></title>
+
+	<link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css');?>">
+</head>
+
+<body>
