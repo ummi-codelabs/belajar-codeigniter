41f58b0422c87ab10562c4584aaf04eebd2d4f5f
Gun Gun Priatna <gungunpriatna02@gmail.com>
2020-10-02 08:08:18 +0700

    materi untuk pertemuan 3

--- codelabs/pertemuan_3/aplikasi_ci/application/models/Mahasiswa_model.php
+++ codelabs/pertemuan_3/aplikasi_ci/application/models/Mahasiswa_model.php
@@ -3,16 +3,16 @@
 class Mahasiswa_model extends CI_Model {
 
 	// atribut
-	public $nim = '1830511108';
-	public $nama = 'Adang'; 
+	public $nim = '1930511041';
+	public $nama = 'Resita'; 
 
-	// method 
+	// method untuk get nim 
 	public function get_nim()
 	{
 		return $this->nim;
-
 	}
 
+	// method untuk get data mahasiswa
 	public function get_mahasiswa()
 	{
 		$data = [
@@ -21,6 +21,5 @@ class Mahasiswa_model extends CI_Model {
 		];
 
 		return $data;
-
 	}
 }
---
+++ codelabs/pertemuan_3/aplikasi_ci/application/views/index_mahasiswa.php
@@ -0,0 +1,2 @@
+<?php 
+echo "nim [tulis nim masing2] dan nama [nama masing2]";
